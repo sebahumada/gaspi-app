1b4def742754edebe2e2ad59eefcf02a032a8fce
Sebastian Ahumada <cbasmx@gmail.com>
2022-01-06 12:02:06 -0300

    analytics

--- src/database/firebase-config.js
+++ src/database/firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "@firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
@@ -16,8 +17,10 @@ const firebaseConfig = {
 
   const app = initializeApp(firebaseConfig);
 
+
   export const auth = getAuth(app);
   export const db = getFirestore(app);
+  export const analytics = getAnalytics(app);
 
   
   
