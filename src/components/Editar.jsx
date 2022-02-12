7f1696acde2d8fc8102878a462fb48e3376a0f93
Sebastian Ahumada <cbasmx@gmail.com>
2022-02-11 23:04:17 -0300

    audit

--- src/components/Editar.jsx
+++ src/components/Editar.jsx
@@ -1,5 +1,5 @@
 
-
+import React from 'react'
 import { doc, getDoc } from 'firebase/firestore';
 
 
--- src/components/FormularioEdit.jsx
+++ src/components/FormularioEdit.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import dayjs from 'dayjs';
 import { useForm } from '../hooks/useForm';
 import  Swal from 'sweetalert2';
